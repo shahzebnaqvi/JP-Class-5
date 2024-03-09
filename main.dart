5e0f67d18e812ee3acf6505791327858c59d136d
Muhammad Faraz <farazmohammad190@gmail.com>
2024-03-09 14:01:16 +0500

    List methods learning

--- main.dart
+++ main.dart
@@ -1,4 +1,19 @@
 main() {
   print("Testing git");
-  print("Learning git");
+  print("List Learning");
+  List stdJp = [98, "shahzeb", "JP"];
+  print(stdJp.reversed);
+  print(stdJp[0] + stdJp[0]);
+  print(stdJp.length);
+  print(stdJp.first);
+  print(stdJp.last);
+  print(stdJp.elementAt(2));
+  print(stdJp);
+  stdJp[1] = "Abdullah";
+  print(stdJp);
+  stdJp.clear();
+  print(stdJp);
+  stdJp.add(75);
+  print(stdJp);
+  print(stdJp.length);
 }
